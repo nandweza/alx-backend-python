ce4ab646399030f4cca843bb22e365606d1c91ee
nandweza <allannandweza@gmail.com>
2022-06-23 12:57:47 +0300

    added task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Basic annotations - floor(float)"""
+
+
+def floor(n: float) -> int:
+    """a type-annotated function floor which takes a float n as
+    argument and returns the floor of the float."""
+    return int(n)
