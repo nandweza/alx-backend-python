872ac349f29d941fd64784e7fcde470e81724152
nandweza <allannandweza@gmail.com>
2022-06-23 13:46:29 +0300

    added task 4

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Define variables with annotation"""
+
+
+from xmlrpc.client import boolean
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: boolean = True
+school: str = 'Holberton'
