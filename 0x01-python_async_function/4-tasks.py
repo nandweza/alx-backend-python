aae1f509960da0b960b88c83ed72bb85f0ba9f03
nandweza <allannandweza@gmail.com>
2022-06-27 21:03:21 +0300

    updates

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -8,17 +8,18 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
     """
     return the list of all the delays(float values).
     """
     queue, array = [], []
 
-    for _ in range(n):
-        queue.append(task_wait_random(max_delay))
+    for i in range(n):
+        task_delay = task_wait_random(max_delay)
+        task_delay.add_done_callback(lambda x: array.append(x.result()))
+        queue.append(task_delay)
 
-    for q in asyncio.as_completed(queue):
-        result = await q
-        array.append(result)
+    for q in queue:
+        await q
 
-    return
+    return array
